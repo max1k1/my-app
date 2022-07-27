09d4147e53f6e707308ffd1eac2fb54874d45d23
max1k1 <ytfgftghcghc@gmail.com>
2022-07-27 18:35:33 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () =>{
+    return <header className="header">
+        <img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1604597937l/55198928.jpg" />
+      </header>
+}
+
+export default Header;
