5fa23d6c9211ac0d63f69d5027cc9a2553ed84ee
max1k1 <63220372+max1k1@users.noreply.github.com>
2022-08-10 20:26:18 +0300

    added dialogs component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import ProfileContent from "./components/ProfileContent/Profile";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-
+import Dialogs from "./components/Dialogs/Dialogs";
 
 
 const App = () => {
@@ -11,9 +11,12 @@ const App = () => {
     <div className="app-wrapper">
       <Header/>
       <Navbar/> 
+      <div className="app-wrapper-content">
+      {/* <Dialogs/> */}
       <ProfileContent/>
+      </div>
     </div>
-  );
+  ); 
 };
 
 export default App;
