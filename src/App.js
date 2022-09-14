f318528e0a127c97ceca38b3dadce3a17f2d9474
max1k1 <63220372+max1k1@users.noreply.github.com>
2022-09-15 00:31:19 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from "react";
 import "./App.css";
-import ProfileContent from "./components/ProfileContent/Profile";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Dialogs from "./components/Dialogs/Dialogs";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./components/ProfileContent/Profile";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className="app-wrapper">
       <Header/>
       <Navbar/> 
       <div className="app-wrapper-content">
-      {/* <Dialogs/> */}
-      <ProfileContent/>
+      <Routes>
+				<Route path="/profile" element = {<Profile />}/>
+				<Route path="/dialogs" element = {<Dialogs />}/>
+        <Route path="/news" element = {<News />}/>
+        <Route path="/music" element = {<Music />}/>
+        <Route path="/settings" element = {<Settings />}/>
+        </Routes>
       </div>
     </div>
+    </BrowserRouter>
   ); 
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,24 @@
 import React from "react";
-import styles from './Navbar.module.css'
+import styles from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () =>{
     return <nav className={styles.nav}>
-    <div className={styles.item}>Profile</div>
-    <div className={styles.item}>Meesages</div>
-    <div className={styles.item}>News</div>
-    <div className={styles.item}>Music</div>
-    <div className={styles.item}>Settings</div>
+    <div className={styles.item}>
+    <NavLink to="/profile" className={navData => navData.isActive?styles.active:styles.item}>Profile</NavLink>
+    </div>
+    <div className={styles.item}>
+      <NavLink to="/dialogs" className={navData => navData.isActive?styles.active:styles.item}>Meesages</NavLink>
+    </div>
+    <div className={styles.item}>
+      <NavLink to="/news" className={navData => navData.isActive?styles.active:styles.item}>News</NavLink>
+    </div>
+    <div className={styles.item}>
+      <NavLink to="/music" className={navData => navData.isActive?styles.active:styles.item}>Music</NavLink>
+      </div> 
+    <div className={styles.item}>
+      <NavLink to="/settings" className={navData => navData.isActive?styles.active:styles.item}>Settings</NavLink>
+    </div>
   </nav>
 }
 
