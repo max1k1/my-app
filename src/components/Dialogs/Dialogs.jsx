3ab104ea22a0c3fb3f517761f277a00da4b956ab
max1k1 <63220372+max1k1@users.noreply.github.com>
2022-10-05 06:34:28 +0300

    new version

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import styles from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
 
 const Dialogs = () => {
   return (
     <div className={styles.dialogs}>
-      <div className={styles.dialogsitems}>
-        <div className={styles.dialog}>Max</div>
-        <div className={styles.dialog}>Sasha</div>
-        <div className={styles.dialog}>Dima</div>
-        <div className={styles.dialog}>Dayan</div>
-        <div className={styles.dialog}>Tristan</div>
+      <div className={styles.dialogsItems}>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/1">Max</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/2">Dima</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/3">Pavel</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/4">Katya</NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to="/dialogs/5">Vinith</NavLink>
+        </div>
       </div>
-      <div className="messages">
-        <div className="message">some message</div>
-        <div className="message">classic</div>
-        <div className="message">dvoe2</div>
+      <div className={styles.messages}>
+        <div className={styles.message}>some message</div>
+        <div className={styles.message}>classic</div>
+        <div className={styles.message}>dvoe2</div>
+        <div className={styles.message}>Walking lonely road</div>
+        <div className={styles.message}>Die alone</div>
       </div>
     </div>
   );
